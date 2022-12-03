15a0af2630f1161c83affcb16a0f5e971e708174
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-03 16:25:17 +0500

    feat: add solution list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const list = async () => {
-    // Write your code here 
+    const folderName = 'files';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const folderPath = join(__dirname, folderName);
+
+    fs.readdir(folderPath, (err, filesList) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        console.log(filesList);
+    });
 };
 
 await list();
