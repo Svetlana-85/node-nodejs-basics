eae57ad2aedacc0894cca7211feae5cc65ff0daf
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-03 11:09:45 +0500

    feat: del console.log

--- src/fs/create.js
+++ src/fs/create.js
@@ -10,7 +10,6 @@ const create = async () => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = dirname(__filename);
     const filePath = join(__dirname, folderName, fileName);
-    console.log(filePath);
     fs.stat(filePath, (err) => {
         if(err) {
             fs.open(filePath, 'w', (err) => {
