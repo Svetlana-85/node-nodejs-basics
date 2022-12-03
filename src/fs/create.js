39f5f116fa0a38b386dd1a1ffbcbbc0eb6a24e56
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-03 10:30:04 +0500

    feat: add solution create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const folderName = 'files';
+const fileName = 'fresh.txt';
+const textFile = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = join(__dirname, folderName, fileName);
+    console.log(filePath);
+    fs.stat(filePath, (err) => {
+        if(err) {
+            fs.open(filePath, 'w', (err) => {
+                if(err) throw new Error('File not created');
+                else {
+                    fs.writeFile(filePath, textFile, (err) => {
+                        if(err) throw new Error('File writing error');
+                    });
+                }
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    });
 };
 
 await create();
