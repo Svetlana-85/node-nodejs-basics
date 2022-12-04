92b3871710740437ee04b9f1b46523e6095d5e68
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-04 15:50:52 +0500

    feat: add solution streams:read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const folderName = 'files';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileName = 'fileToRead.txt';
+    const filePath = join(__dirname, folderName, fileName);
+
+    const stream = new fs.ReadStream(filePath, 'utf8');
+    let data = '';
+    stream.on('data', chunk => data += chunk);
+    stream.on('close', () => process.stdout.write(data));
 };
 
 await read();
