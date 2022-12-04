96c5ca10aa53df63d8488bbda722868d893e1f41
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-04 23:43:04 +0500

    feat: add solution streams:write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const folderName = 'files';
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const fileName = 'fileToWrite.txt';
+    const filePath = join(__dirname, folderName, fileName);
+
+    const streamWrite = fs.createWriteStream(filePath, 'utf-8');
+    process.stdin.pipe(streamWrite);
 };
 
 await write();
