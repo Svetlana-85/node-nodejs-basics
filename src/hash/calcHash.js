b72b1d6aebea528ff866ee144c1fe05b21fcc486
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-09 20:20:03 +0500

    feat: change solution calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -14,7 +14,7 @@ const calculateHash = async () => {
         if (err) {
             throw new Error('FS operation failed');
         }
-        const hash = crypto.createHash('sha256').update(data).digest('hest');
+        const hash = crypto.createHash('sha256').update(data).digest('hex');
         console.log(hash);
     })
 };
