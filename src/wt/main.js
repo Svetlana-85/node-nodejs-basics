483c7f1ad83bc0dc0e5a9ed464467f52daed05aa
Svetlana-85 <Svetlanka-85@yandex.ru>
2022-12-09 20:19:23 +0500

    feat: add solution worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'node:worker_threads';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'node:os';
+
+const fileName = 'worker.js';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const filePath = join(__dirname, fileName);
+const numberStart = 10;
+const numCPUs = cpus().length; 
+
 const performCalculations = async () => {
-    // Write your code here
+    const arrPromise = [];
+    for(let i = 0; i < numCPUs; i++) {
+        arrPromise[i] = new Promise((resolve) => {
+            const worker = new Worker(filePath, { workerData: numberStart + i });
+            worker.on('message', res => resolve({status: 'resolved', res}));
+            worker.on('error', () => resolve({status: 'error', res: null}));
+        });
+    }
+
+    await Promise.all(arrPromise).then(val => {
+        console.log(val);        
+    });
 };
 
 await performCalculations();
